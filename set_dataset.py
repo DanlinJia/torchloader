7d4e6d5a47d7d267a4deda89328c7130f33172ee
Danlin <jdlsx21499@gmail.com>
2021-09-27 17:36:29 -0400

    add missed lib

---
+++ set_dataset.py
@@ -0,0 +1,111 @@
+import os
+import torch.utils.data
+import torch.utils.data.distributed
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+
+def get_dataset(args):
+
+    #############################################################
+    # ------------------ cifar-10 ------------------------------
+
+    if args.dataset == "cifar10":
+        train_dataset = datasets.CIFAR10('./data.cifar10', train=True, download=True,
+                                         transform=transforms.Compose([
+                                             transforms.Pad(4),
+                                             transforms.RandomCrop(32),
+                                             transforms.RandomHorizontalFlip(),
+                                             transforms.ToTensor(),
+                                             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                                         ]))
+
+        if args.distributed:
+            train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+        else:
+            train_sampler = None
+
+        train_loader = torch.utils.data.DataLoader(
+            train_dataset, batch_size=args.batch_size, shuffle=(train_sampler is None),
+            num_workers=args.workers, pin_memory=True, sampler=train_sampler)
+
+        test_loader = torch.utils.data.DataLoader(
+            datasets.CIFAR10('./data.cifar10', train=False, transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+            ])),
+            batch_size=256, shuffle=True, num_workers=1, pin_memory=True)
+
+
+    #############################################################
+    # ------------------ cifar-100 ------------------------------
+
+    elif args.dataset == "cifar100":
+        train_dataset = datasets.CIFAR100('./data.cifar100', train=True, download=True,
+                                          transform=transforms.Compose([
+                                              transforms.Pad(4),
+                                              transforms.RandomCrop(32),
+                                              transforms.RandomHorizontalFlip(),
+                                              transforms.ToTensor(),
+                                              transforms.Normalize(
+                                                  (0.5070751592371323, 0.48654887331495095, 0.4409178433670343),
+                                                  (0.2673342858792401, 0.2564384629170883, 0.27615047132568404))
+                                          ]))
+
+        if args.distributed:
+            train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+        else:
+            train_sampler = None
+
+        train_loader = torch.utils.data.DataLoader(
+            train_dataset, batch_size=args.batch_size, shuffle=(train_sampler is None),
+            num_workers=args.workers, pin_memory=True, sampler=train_sampler)
+
+        test_loader = torch.utils.data.DataLoader(
+            datasets.CIFAR100('./data.cifar100', train=False, transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.5070751592371323, 0.48654887331495095, 0.4409178433670343),
+                                     (0.2673342858792401, 0.2564384629170883, 0.27615047132568404))
+            ])),
+            batch_size=256, shuffle=True, num_workers=1, pin_memory=True)
+
+    #############################################################
+    # ------------------ imagenet ------------------------------
+
+    else:
+        # Data loading code
+        traindir = os.path.join(args.data, 'train')
+        valdir = os.path.join(args.data, 'val')
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                         std=[0.229, 0.224, 0.225])
+
+        train_dataset = datasets.ImageFolder(
+            traindir,
+            transforms.Compose([
+                transforms.RandomResizedCrop(224),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                normalize,
+            ]))
+
+        if args.distributed:
+            train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+        else:
+            train_sampler = None
+
+        train_loader = torch.utils.data.DataLoader(
+            train_dataset, batch_size=args.batch_size, shuffle=(train_sampler is None),
+            num_workers=args.workers, pin_memory=True, sampler=train_sampler)
+
+        test_loader = torch.utils.data.DataLoader(
+            datasets.ImageFolder(valdir, transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                normalize,
+            ])),
+            batch_size=args.batch_size, shuffle=False,
+            num_workers=args.workers, pin_memory=True)
+
+
+    return train_loader, test_loader, train_sampler
